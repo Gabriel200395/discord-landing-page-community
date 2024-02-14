67884c4470577d04fae46953d7196a9c785f9a6d
gabrielwansoft <gabriel.sousa@wansoft.com.br>
2024-02-14 16:00:36 -0300

    implementando container de invite

--- src/App.vue
+++ src/App.vue
@@ -29,10 +29,25 @@
       </p>
 
       <div class="max-w-lg flex space-x-3">
-        <button class="bg-white w-60 rounded-full flex items-center justify-center h-10 text-sm">Download for Windows</button>
-        <button class="bg-[#23272A] w-60 rounded-full flex items-center justify-center h-10 text-white text-sm">Open Discord in your browser</button>
+        <button class="bg-white w-60 rounded-full flex items-center justify-center h-10 text-sm">Download for
+          Windows</button>
+        <button class="bg-[#23272A] w-60 rounded-full flex items-center justify-center h-10 text-white text-sm">Open
+          Discord in your browser</button>
       </div>
     </div>
+  </section>
+
+  <section class="px-10 mt-12 flex justify-center w-full">
+
+    <div class="flex">
+      <img src="../src/assets/imgs/create_invite.svg" alt="invite-img" class="pr-10">
+      <div>
+        <h3 class="text-3xl max-w-48  text-[#23272A] font-medium mb-5">Create an invite-only place where you belong</h3>
+        <p class="max-w-60 text-base text-[#23272A]">Discord servers are organized into topic-based channels where you can
+          collaborate, share, and just talk about your day without clogging up a group chat.</p>
+      </div>
+    </div>
+
   </section>
 </template>
 
