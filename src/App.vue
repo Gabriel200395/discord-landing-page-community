8280de1c0e564dc69dd042eb78993081c0c135c8
gabrielwansoft <gabriel.sousa@wansoft.com.br>
2024-02-20 14:37:50 -0300

    implementando container reliable tech

--- src/App.vue
+++ src/App.vue
@@ -59,18 +59,34 @@
 
       <img src="../src/assets/imgs/voice_connected.svg" alt="voice-connected" class="h-96">
     </div>
-  </section> 
- 
+  </section>
+
   <section class="px-10 mt-12 flex justify-center w-full">
     <div class="max-w-6xl w-full flex justify-between py-10 px-10 items-center">
-      <img src="../src/assets/imgs/from_to_fandom.svg" alt="from-to-fandom" class="h-96"> 
-
+      <img src="../src/assets/imgs/from_to_fandom.svg" alt="from-to-fandom" class="h-96">
       <div>
         <h3 class="text-3xl max-w-60 text-[#23272A] font-medium mb-5">From few to a fandom</h3>
-        <p class="max-w-60 text-base text-[#23272A]">Get any community running with moderation tools and custom member access. Give members special powers, set up private channels, and more.</p>
+        <p class="max-w-60 text-base text-[#23272A]">Get any community running with moderation tools and custom member
+          access. Give members special powers, set up private channels, and more.</p>
       </div>
     </div>
-  </section> 
+  </section>
 
+  <section class="flex justify-center met-12 px-10 w-full">
+    <div class="flex flex-col bg-[#F6F6F6] max-w-6xl justify-center items-center py-20 text-center">
+      <div>
+        <h3 class="text-[#23272A]  text-3xl font-medium mb-5">Reliable tech for staying close</h3>
+        <p class="text-sm max-w-2xl">Low-latency voice and video feels like you’re in the same room. Wave hello over video, watch friends stream
+          their games, or gather up and have a drawing session with screen share.</p>
+      </div>
+
+      <img src="../src/assets/imgs/reliable_tech .svg" alt="reliable_tech">
+
+      <div>
+        <p class="text-[#23272A] text-xl font-medium mb-5">Ready to start your journey?</p>
+        <button class="bg-[#5865F2] text-white w-60 h-10 rounded-full">Download for Windows</button>
+      </div>
+    </div>
+  </section>
 </template>
 
