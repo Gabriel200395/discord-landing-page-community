5e2a70acf7142b5f6904ad75d4fbd70369abd9bb
gabrielwansoft <gabriel.sousa@wansoft.com.br>
2024-02-20 14:21:40 -0300

    implementando container from to fandom

--- src/App.vue
+++ src/App.vue
@@ -59,6 +59,18 @@
 
       <img src="../src/assets/imgs/voice_connected.svg" alt="voice-connected" class="h-96">
     </div>
-  </section>
+  </section> 
+ 
+  <section class="px-10 mt-12 flex justify-center w-full">
+    <div class="max-w-6xl w-full flex justify-between py-10 px-10 items-center">
+      <img src="../src/assets/imgs/from_to_fandom.svg" alt="from-to-fandom" class="h-96"> 
+
+      <div>
+        <h3 class="text-3xl max-w-60 text-[#23272A] font-medium mb-5">From few to a fandom</h3>
+        <p class="max-w-60 text-base text-[#23272A]">Get any community running with moderation tools and custom member access. Give members special powers, set up private channels, and more.</p>
+      </div>
+    </div>
+  </section> 
+
 </template>
 
