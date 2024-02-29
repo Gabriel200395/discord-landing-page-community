74b5f3362b436672be8cd93205e86f2172b5c718
gabrielwansoft <gabriel.sousa@wansoft.com.br>
2024-02-29 11:46:37 -0300

    criando pasta de contants

---
+++ src/constants/languages.ts
@@ -0,0 +1,116 @@
+export const languages = {
+    EN: {
+      menus: ['Baixa', 'Nitro', 'Descobrir', 'Segurança', 'Suporte', 'Blog'],
+      information: {
+        titulo: 'IMAGINE A PLACE...',
+        text: '...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.',
+        buttons: ['Download for Windows', 'Open Discord in your browser'],
+      },
+      createInvite: {
+        titulo: 'Create an invite-only place where you belong',
+        text: 'Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.',
+        url: '../../../src/assets/imgs/create_invite.svg'
+      },
+      voiceConnected: {
+        title: 'Where hanging out is easy',
+        text: 'Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.',
+        url: '../../../src/assets/imgs/voice_connected.svg'
+      },
+      fromToFandom: {
+        title: 'Where hanging out is easy',
+        text: 'Get any community running with moderation tools and custom member access. Give members special powers, set up private channels, and more.',
+        url: '../../../src/assets/imgs/from_to_fandom.svg'
+      },
+      reliableTech: {
+        title: 'Reliable tech for staying close',
+        text: 'Low-latency voice and video feels like you’re in the same room. Wave hello over video, watch friends stream their games, or gather up and have a drawing session with screen share.',
+        url: '../../../src/assets/imgs/reliable_tech .svg',
+        journey: {
+          title: 'Pronto para começar sua jornada?',
+          text: 'Baixar para Windows'
+        }
+      },
+      footer: {
+        list: [
+          {
+            id: 1,
+            title: 'Product',
+            list: ['Download', 'Nitro', 'Status']
+          },
+          {
+            id: 2,
+            title: 'Company',
+            list: ['About', 'Jobs', 'Branding', 'Newsroom']
+          },
+          {
+            id: 3,
+            title: 'Resources',
+            list: ['College', 'Support', 'Safety', 'Blog', 'Feedback', 'Developers', 'StreamKit']
+          },
+          {
+            id: 4,
+            title: 'Policies',
+            list: ['Terms', 'Privacy', 'Cookie Settings', 'Guidelines', 'Acknowledgements', 'Licenses', 'Moderation']
+          },
+        ],
+        buttonText: 'Open Discord'
+      }
+    },
+    PT: {
+      menus: ['Baixa', 'Nitro', 'Descobrir', 'Segurança', 'Apoiar', 'Blogue'],
+      information: {
+        titulo: 'IMAGINE UM LUGAR...',
+        text: '....onde você pode pertencer a um clube escolar, a um grupo de jogos ou a uma comunidade artística mundial. Onde apenas você e alguns amigos podem passar algum tempo juntos. Um lugar que facilita conversar todos os dias e sair com mais frequência.',
+        buttons: ['Baixar para Windows', 'Abra o Discord no seu navegador'],
+      },
+      createInvite: {
+        titulo: 'Crie um lugar somente para convidados ao qual você pertence',
+        text: 'Os servidores Discord são organizados em canais baseados em tópicos onde você pode colaborar, compartilhar e apenas falar sobre o seu dia sem atrapalhar um bate-papo em grupo.',
+        url: '../../../src/assets/imgs/create_invite.svg'
+      },
+      voiceConnected: {
+        title: 'Onde sair é fácil',
+        text: 'Sente-se em um canal de voz quando estiver livre. Amigos em seu servidor podem ver que você está por perto e entrar instantaneamente para conversar, sem precisar ligar.',
+        url: '../../../src/assets/imgs/voice_connected.svg'
+      },
+      fromToFandom: {
+        title: 'De poucos a um fandom',
+        text: 'Faça qualquer comunidade funcionar com ferramentas de moderação e acesso personalizado para membros. Dê aos membros poderes especiais, crie canais privados e muito mais.',
+        url: '../../../src/assets/imgs/from_to_fandom.svg'
+      },
+      reliableTech: {
+        title: 'Tecnologia confiável para ficar por perto',
+        text: 'Voz e vídeo de baixa latência dão a sensação de que você está na mesma sala. Dê um alô por vídeo, assista aos amigos transmitirem seus jogos ou reúna-se e faça uma sessão de desenho com compartilhamento de tela.',
+        url: '../../../src/assets/imgs/reliable_tech .svg',
+        journey: {
+          title: 'Pronto para começar sua jornada?',
+          text: 'Baixar para Windows'
+        }
+      },
+      footer: {
+        list: [
+          {
+            id: 1,
+            title: 'produtos',
+            list: ['Download', 'Nitro', 'Status']
+          },
+          {
+            id: 2,
+            title: 'Empresa',
+            list: ['Sobre', 'Empregos', 'Marca', 'Redação']
+          },
+          {
+            id: 3,
+            title: 'Recursos',
+            list: ['Faculdade', 'Apoiar', 'Segurança', 'Blogue', 'Opinião', 'Desenvolvedores', 'StreamKit']
+          },
+          {
+            id: 4,
+            title: 'Políticas',
+            list: ['Termos', 'Privacidade', 'Configurações de cookies', 'Diretrizes', 'Reconhecimentos', 'Licenças', 'Moderação']
+          },
+        ],
+        buttonText: 'Abrir discórdia'
+      }
+    }
+  }
